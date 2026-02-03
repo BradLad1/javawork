8f948b19160622ffc59369996bf7a02c3a217c73
BradLad1 <148502451+BradLad1@users.noreply.github.com>
2026-02-03 00:11:50 +0000

    Add files via upload

---
+++ JavaIntro.java
@@ -0,0 +1,16 @@
+public class JavaIntro {
+    public static  void main(String[] args){
+        //String[] args is need inorder
+        // for our program to run
+
+        //System.out.print("I like pizza\n");
+        System.out.println("I like pizza");//println prints on a differline
+        //print will print on one line
+        System.out.println("Its nice");
+        //is a comment
+
+        /*This
+        * is a multi line comment*/
+
+    }
+}
